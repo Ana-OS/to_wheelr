1712089b4ead37c4c3c543b32bb717e33671c730
ana sanots <anasoliveiraaa@gmail.com>
2022-06-08 17:40:18 -0400

    bicycle model

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,9 +1,4 @@
 class Bicycle < ApplicationRecord
   belongs_to :user
 
-  validates :brand, :name, :price_day, :bicycle_type, :location, :number_wheels, presence: true
-  validates :name, uniqueness: true
-  validates :bicycle_type, inclusion: {in: %w(tandem regular monocycle tricycle elliptical)}
-  validates :price_day, numericality: { only_integer: true}
-  validates :number_wheels, numericality: { only_integer: true}
 end
