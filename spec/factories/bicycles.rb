f2409a5d07c3f7080432eab5a58c23e934a82e0b
ana sanots <anasoliveiraaa@gmail.com>
2022-06-09 12:09:24 -0400

    factories for user and bicyle

--- spec/factories/bicycles.rb
+++ spec/factories/bicycles.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :bicycle do
-
+    brand {Faker::App.name}
+    name {Faker::Artist.unique.name}
+    location {"Austin"}
+    price_day {40}
+    number_wheels {2}
+    bicycle_type {"tandem"}
+    foldable {[true, false].sample}
+    user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :user do
-    
+    email {Faker::Internet.email}
+    password {123456}
+
+    transient do
+      qtt_bicycles {2}
+    end
+
+    trait :with_bicycles do
+      after(:build) do |user, evaluator|
+        create_list(:bicycle, evaluator.qtt_bicycles, user: user)
+      end
+    end
   end
 end
