78e968d871becc764433c73ee9fb0e3d8dcddde7
ana sanots <anasoliveiraaa@gmail.com>
2022-06-10 15:05:26 -0400

    routes and controller for bicycles api

---
+++ app/controllers/api/v1/bicycles_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::BicyclesController < Api::V1::BaseController
+  before_action :set_bicycle, only: [ :show, :update, :destroy]
+  # skip_before_action :authenticate_user!, except: :index
+  def index
+    @bicycles = Bicycle.all
+  end
+
+  def show; end
+
+  def create
+    @bicycle = Bicycle.new(bicycle_params)
+    @bicycle.user = current_user
+    if @bicycle.save
+      render :show, status: :created
+    else
+      render json: @bicycle.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if  @bicycle.update(bicycle_params)
+      render :show, status: :created
+    else
+      render json: @bicycle.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bicycle.destroy
+    head :no_content
+  end
+
+
+  private
+
+  def bicycle_params
+    params.require(:bicycle).permit(:name, :brand, :name, :price_day, :bicycle_type, :location, :number_wheels)
+  end
+
+  def set_bicycle
+    @bicycle = Bicycle.find(params[:id])
+  end
+end
