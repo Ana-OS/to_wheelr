7ab7d89f53ab74fcd66c9e1c76edf88413018f1f
ana sanots <anasoliveiraaa@gmail.com>
2022-06-11 17:35:30 -0400

    schema matchers added ffor index and show

---
+++ app/views/api/v1/bicycles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.bicycle @bicycle
--- spec/requests/bicycles_spec.rb
+++ spec/requests/bicycles_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Bicycles", type: :request do
   describe "GET /index" do
+    before { get '/api/v1/bicycles' }
 
+    it "returns the correct json" do
+      expect(json).to match_json_schema("index")
+    end
   end
 end
