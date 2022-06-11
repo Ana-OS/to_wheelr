014cb1ec59e331e1e45be62e1d73b8a8c4256eb7
ana sanots <anasoliveiraaa@gmail.com>
2022-06-11 14:43:41 -0400

    seeding bicycles and users

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,7 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
   config.include FactoryBot::Syntax::Methods
+  config.include ApiHelpers
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
---
+++ spec/requests/bicycles_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Bicycles", type: :request do
+  describe "GET /index" do
+
+  end
+end
