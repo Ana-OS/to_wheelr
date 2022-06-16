7d9e71248314f8ed93e1f04f7e5911637dd8862d
ana sanots <anasoliveiraaa@gmail.com>
2022-06-16 10:10:15 -0400

    ctach all route for requests that are not met

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "api/v1/bicycles#index", defaults: { format: :json }
+  root "pages#index"
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :bicycles do
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
     end
   end
 
+  # route requests that dont match any api route to the root path
+  get "*path", to: "pages#index", via: :all
 end
